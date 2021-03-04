745ea455b67516f3f01cc1b38abaa71452bb2c73
Bronson Schultz <58102397+BronsonSchultz@users.noreply.github.com>
2021-03-04 14:53:34 -0600

    finshed parent and grandparent

--- src/main/scala/problem1.scala
+++ src/main/scala/problem1.scala
@@ -3,6 +3,7 @@ Bronson Schultz, 11231230, bcs269
 CMPT 340 A3 Q1
  */
 case object problem1 {
+
   val royalParent = Map("August" -> ("m", "Eugenie", "Jack"), "George" -> ("m", "William", "Catherine"),
   "Charlotte" -> ("f", "William", "Catherine"), "Louis" -> ("m", "William", "Catherine"),
   "Archie" -> ("m", "Harry", "Meghan"), "Savannah" -> ("f", "Autumn", "Peter"),
@@ -16,7 +17,7 @@ case object problem1 {
   "Sarah" -> ("f", "", ""), "Mike" -> ("m", "", ""), "Autumn" -> ("f", "", ""), "Meghan" -> ("f", "", ""),
   "Catherine" -> ("f", "", ""), "Timothy" -> ("m", "", ""), "Jack" -> ("m", "", ""), "Camilla" -> ("f", "", ""))
 
-  def parent(p: String): Either[String, (String, String)] = {
+  def parents(p: String): Either[String, (String, String)] = {
     royalParent.get(p) match {
       case Some((_,"","")) => Left(p + " has no parental info")
       case Some((_,m,d)) => Right((m,d))
@@ -25,25 +26,57 @@ case object problem1 {
   }
 
   def grandparents(p: String): Either[String, List[String]] = {
-    val parents = parent(p)
-    parents match {
-      case Right((m,d)) =>
-        val mps = parent(m)
-        val dps = parent(d)
-        println("mom's parents " + mps.getOrElse(Left(p+"'s mom " + m + "has no parental info")))
-        println("Dad's parents " + dps.getOrElse(Left(p+"'s dad "+ d + "has no parental info")))
-        Right(List())
+    val parent = parents(p)
+    parent match {
+      case Right((m: String,d: String)) =>
+        val mps = parents(m).getOrElse(("",""))
+        val dps = parents(d).getOrElse(("",""))
+
+        Right(List(mps._1, mps._2, dps._1, dps._2).filterNot(_ == ""))
+      case Left(error) => Left(error)
     }
   }
 
-  def main(args: Array[String]): Unit = {
-    println("parents of August should be (Eugenie,Jack")
-    println(parent("August"))
-    println("parents of 123 should be a Left")
-    println(parent("123"))
-    println("parents of Philip should be a Left")
-    println(parent("Philip"))
+  def siblings(p: String): Either[String, List[String]] = {
+    val parent = parents(p)
+    lazy val sibs = LazyList()
+    parent match {
+      case Right((m: String, d: String)) =>
+        for (person <- royalParent.keys) {
+          val a = parents(person).getOrElse(("",""))
+          if (a == (m,d) || a == (d,m)) {
+            sibs.prepended(person)
+          }
 
+        }
+        Right(sibs.toList)
+      case Left(error) => Left(error)
+    }
+  }
+  def main(args: Array[String]): Unit = {
+    println("parents of August should be (Eugenie,Jack)")
+    println(parents("August"))
+    println("--------------------")
+
+    println("parents of 123 should be a Left")
+    println(parents("123"))
+    println("--------------------")
+
+    println("parents of Philip should be a Left")
+    println(parents("Philip"))
+    println("--------------------")
+
+    println()
+
+    println("August's grandparents should be Andrew and Sarah")
     println(grandparents("August"))
+    println("--------------------")
+
+    println("Elizabeth's grandparents don't exist, pass through the error from parents()")
+    println(grandparents("Elizabeth"))
+    println("--------------------")
+
+    println(siblings("George"))
+
   }
 }
